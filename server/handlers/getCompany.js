a2a864e45c4da93b1c1f4e8735d8b78618b9aea3
“JM <djeehem@gmail.com>
2021-11-30 07:22:21 -0500

    getCompany-endpoint

---
+++ server/handlers/getCompany.js
@@ -0,0 +1,47 @@
+// const path = require('path')
+// require('dotenv').config({path:__dirname+'../../.env'});
+// const {MONGO_URI} = process.env;
+
+const { MongoClient } = require("mongodb");
+
+const URI = "mongodb+srv://djeehem:j43j5h345hmnb@cluster0.alqgy.mongodb.net/e-commerce?retryWrites=true&w=majority"
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const client = new MongoClient(URI, options);
+
+const getCompany = async (req, res) => {
+  const {companyId } = req.params
+  const query = Number(companyId)
+
+  try {
+    await client.connect();
+    console.log("connected");
+  
+    const db = client.db("e-commerce");
+    const companyInfo = await db.collection("companies").findOne({ _id: query })
+
+    companyInfo ?
+      res.status(200).json({
+        status: 200,
+        data: companyInfo,
+        message: "Company retrieved"
+      }) :
+      res.status(400).json({
+        status: 400,
+        message: 'Company could not be retrieved'
+      })
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "Server issue" });
+  }
+  finally {
+    client.close();
+  }
+};
+
+module.exports = { getCompany };
