5f890f752c25bfebfd24c7a8fdb73ce3d1540f9f
Bridget Walsh <bridget.walsh@mail.mcgill.ca>
2021-11-30 10:41:19 -0500

    rendered homepage

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,13 +1,44 @@
 import React from "react";
+import styled from "styled-components";
 
-const Item = () => {
+const Item = ({ name, category, picture }) => {
   return (
-    <div>
-      <ul>Items go here</ul>
-      <li>Much Sales</li>
-      <li>Wow</li>
-    </div>
+    <SmallerBox>
+      <Image src={picture} alt="gadget" />
+      <Div>
+        <Title>{name}</Title>
+        <Category>{category}</Category>
+      </Div>
+    </SmallerBox>
   );
 };
 
 export default Item;
+const Title = styled.h2`
+  margin-bottom: 100px;
+  min-height: 64px;
+`;
+
+const Image = styled.img`
+  /* width: 100%; */
+  max-height: 150px;
+`;
+
+const SmallerBox = styled.div`
+  display: inline-block;
+  margin: 30px;
+  background-color: white;
+  width: 500px;
+  height: 375px;
+  &:hover {
+    border: solid;
+    border-color: black;
+    border-width: 0.5px;
+  }
+`;
+
+const Div = styled.div`
+  margin-left: 20px;
+`;
+
+const Category = styled.p``;
